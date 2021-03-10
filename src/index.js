c61ef98e8117814ce12d2c5fc399d3fe17ac2501
raulfranck <raulengfranck@gmail.com>
2021-03-09 21:01:37 -0300

    Aplicando middleware

--- src/index.js
+++ src/index.js
@@ -6,6 +6,21 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyExistsAccountCPF(request, response, next) {
+    const { cpf } = request.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return response.status(400).json({error: "Customer not found"});
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
     const {cpf, name} = request.body;
 
@@ -26,15 +41,8 @@ app.post("/account", (request, response) => {
 
 });
 
-app.get("/statement", (request, response) => {
-    const { cpf } = request.headers;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
-    if (!customer) {
-        return response.status(400).json({error: "Customer not found"});
-    }
-
+app.get("/statement", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
     return response.json(customer.statement)
 })
 
