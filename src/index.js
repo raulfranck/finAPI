5ab8ffaa867917212c6252bf43fea1a04d382198
raulfranck <raulengfranck@gmail.com>
2021-03-10 16:50:07 -0300

    Atualizando conta PUT

--- src/index.js
+++ src/index.js
@@ -111,4 +111,18 @@ app.get("/statement/date", verifyExistsAccountCPF, (request, response) => {
     return response.json(statement);
 })
 
+app.put("/account", verifyExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send()
+})
+
+app.get("/account", verifyExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+})
 app.listen(3333);
